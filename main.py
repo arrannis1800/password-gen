81037b09385f56701e26b4677e6eb6df813d0d21
arrannis1800 <52630886+arrannis1800@users.noreply.github.com>
2022-10-09 19:54:16 +0300

    Some bugfixes and added showing list of password

--- main.py
+++ main.py
@@ -10,6 +10,34 @@ def create_csv():
         with open('passwords.csv', 'w') as file:
             writer = csv.writer(file)
             writer.writerow(headers)
+        print('''>> CSV file created \n''')
+
+
+def main_menu():
+    print('''Hello, It's password gen. Few questions before\n''')
+
+    while True:
+        main_menu = input(
+            '''\nWhat do you want to do?\n1. Create a new pass\n2. Show passwords\n\nq - for quit\n>> ''').lower()
+        if main_menu in ('q', 'quit'):
+            print('''Good bye!''')
+            break
+        elif main_menu in ('1', 'create'):
+            num_char, chars, upper_chars, spec_chars = asking_params()
+            password_gened = generate_pass(num_char=num_char, chars=chars, upper_chars=upper_chars,
+                                           spec_chars=spec_chars)
+            while True:
+                saving = input('''Do you want to save it? [Y|n]\n>> ''').lower()
+                if saving in ('y', 'yes'):
+                    save_pass(password_gened)
+                    break
+                elif saving in ('n', 'no'):
+                    print('''Ok''')
+                    break
+                else:
+                    print('''Can't understand. Please write Yes or No''')
+        elif main_menu in ('2', 'show'):
+            show_pass()
 
 
 def asking_params():
@@ -59,16 +87,16 @@ def asking_params():
 
 
 def generate_pass(num_char=20, chars=True, upper_chars=True, spec_chars=True):
-    acceptable_chars = [i for i in range(48, 58)]               # nums
+    acceptable_chars = [i for i in range(48, 58)]  # nums
     if chars:
-        acceptable_chars += [i for i in range(97, 123)]         # lowercase letters
+        acceptable_chars += [i for i in range(97, 123)]  # lowercase letters
     if upper_chars:
-        acceptable_chars += [i for i in range(65, 91)]          # uppercase letter
+        acceptable_chars += [i for i in range(65, 91)]  # uppercase letter
     if spec_chars:
         acceptable_chars += [i for i in range(34, 48)] \
                             + [i for i in range(58, 65)] \
                             + [i for i in range(91, 97)] \
-                            + [i for i in range(123, 127)]      # special characters
+                            + [i for i in range(123, 127)]  # special characters
 
     password = ''.join(chr(int(random.choice(acceptable_chars))) for i in range(num_char))
     print('''Your password is >>>''', password)
@@ -92,34 +120,47 @@ def save_pass(password):
         writer = csv.writer(file)
         writer.writerow(new_row)
 
+    print('''It was saved in csv file''')
+
+
+def show_pass():
+    print('''\nYour passwords list''')
+    with open("passwords.csv", 'r') as file:
+        reader = file.readlines()
+        pages = int(-(-((len(reader) - 2) / 10) // 1))
+        cur_page = 0
+        while True:
+            print(reader[0].replace(',', '   |  '))
+            for row in reader[cur_page * 10 + 2:min(cur_page * 10 + 12, len(reader))]:
+                print(row.replace(',', '   |  '), end='')
+            if pages > 1:
+                print(f'''\nIt's {cur_page + 1} page of {pages}''')
+                string = ''
+                if cur_page != 0:
+                    string += '''For move on previous page type "<". '''
+                if cur_page != pages - 1:
+                    string += '''For move on next page type ">". '''
+                string += '''\nTo return to main menu type "q". \n>> '''
+                action = input(string).lower()
+                match action:
+                    case 'q':
+                        break
+                    case '>':
+                        if cur_page != pages - 1: cur_page += 1
+                    case '<':
+                        if cur_page != 0: cur_page -= 1
+                    case _:
+                        print('''Please, try again''')
+                continue
+            break
+
 
 def main():
-    create_csv()
-
+    create_csv()  # if not exist, create csv file for passwords
     os.system("cls")
-    print('''Hello, It's password gen. Few questions before\n''')
-
-    while True:
-        main_menu = input('''\nWhat do you want to do?\n1. Create a new pass\n\nq - for quit\n>> ''').lower()
-        if main_menu in ('q', 'quit'):
-            print('''Good bye!''')
-            break
-        elif main_menu in ('1', 'pass', 'password', 'create'):
-            num_char, chars, upper_chars, spec_chars = asking_params()
-            password_gened = generate_pass(num_char=num_char, chars=chars, upper_chars=upper_chars,
-                                           spec_chars=spec_chars)
-            while True:
-                saving = input('''Do you want to save it? [Y|n]\n>> ''').lower()
-                if saving in ('y', 'yes'):
-                    save_pass(password_gened)
-                    print('''It was saved in csv file''')
-                    break
-                elif saving in ('n', 'no'):
-                    print('''Ok''')
-                    break
-                else:
-                    print('''Can't understand. Please write Yes or No''')
+    main_menu()  # opens main menu
 
 
 if __name__ == '__main__':
     main()
+    os.system("cls")
